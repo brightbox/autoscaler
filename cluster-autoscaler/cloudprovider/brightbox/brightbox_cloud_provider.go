58262a9c5e3d73ef57bb703078c4f3a601d06a15
Neil Wilson <neil@aldur.co.uk>
2019-12-31 17:41:32 +0000

    Move k8sdsk to new repo

--- cluster-autoscaler/cloudprovider/brightbox/brightbox_cloud_provider.go
+++ cluster-autoscaler/cloudprovider/brightbox/brightbox_cloud_provider.go
@@ -22,8 +22,8 @@ import (
 	"regexp"
 	"strings"
 
-	"github.com/brightbox/brightbox-cloud-controller-manager/k8ssdk"
 	brightbox "github.com/brightbox/gobrightbox"
+	"github.com/brightbox/k8ssdk"
 	apiv1 "k8s.io/api/core/v1"
 	"k8s.io/apimachinery/pkg/api/resource"
 	"k8s.io/autoscaler/cluster-autoscaler/cloudprovider"
