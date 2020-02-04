254d12613399bca3fa1ca710ec3458c40e1037be
Neil Wilson <neil@aldur.co.uk>
2020-02-04 15:27:38 +0000

    Switch to using patched helm to deploy

---
+++ cluster-autoscaler/cloudprovider/brightbox/examples/fix_cluster_autoscaler.rb
@@ -0,0 +1,24 @@
+require 'yaml'
+
+result = []
+YAML.load_stream(ARGF) do |resource|
+  case resource['kind']
+  when "Service"
+    next
+  when "ClusterRole"
+    resource['rules'] << 
+      { 
+        "apiGroups" => ["coordination.k8s.io"],
+        "resources" => ["leases"],
+        "verbs" => ["create"]
+      } <<
+      {
+        "apiGroups" => ["coordination.k8s.io"],
+        "resourceNames" => ["cluster-autoscaler"],
+        "resources" => ["leases"],
+        "verbs" => ["get", "update"]
+      }
+  end
+  result << resource
+end
+print YAML.dump_stream(*result)
